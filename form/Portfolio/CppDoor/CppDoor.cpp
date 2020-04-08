a293b17ca6cc77bdf73778b9db60d63caefc2cab
ToyAuthor <eye5002003@gmail.com>
2020-04-08 11:30:54 +0800

    新增一個新的library供C#使用

---
+++ form/Portfolio/CppDoor/CppDoor.cpp
@@ -0,0 +1,6 @@
+// 這是主要 DLL 檔案。
+
+#include "stdafx.h"
+
+#include "CppDoor.h"
+
---
+++ form/Portfolio/CppDoor/CppDoor.h
@@ -0,0 +1,13 @@
+// CppDoor.h
+
+#pragma once
+
+using namespace System;
+
+namespace CppDoor {
+
+	public ref class Class1
+	{
+		// TODO:  在此加入這個類別的方法。
+	};
+}
